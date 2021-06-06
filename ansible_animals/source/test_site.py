8510a024028324c08e151cca2d683e02e4efc360
Antosha <cmex1966@gmail.com>
2021-06-06 16:38:54 +0300

    Added user data validation

--- ansible_animals/source/test_site.py
+++ ansible_animals/source/test_site.py
@@ -1,6 +1,6 @@
 import socket
 import json
-import re
+#import re
 from flask import Flask, request
 
 file_name="converted_emodji"
@@ -9,16 +9,17 @@ with open(file_name) as f:
     lines = f.read().strip().split('\n')
     res = []
     for i in lines:
-        res.append(re.sub(r'(\S+): (\S+)', r'\1:\2', i).split(':'))
+        #res.append(re.sub(r'(\S+): (\S+)', r'\1:\2', i).split(':'))
+        res.append(i.split(':'))
     emoji_dict = dict(res)
 
 def animal_say(animal, sound, count, heart):
-    test_ret=""
+    result=""
     for i in range(int(count)):
-        test_ret+=f'{animal} say {sound}\n'
-    #print(test_ret)
-    test_ret+=f'Made with {heart} by Antosha\n'
-    return test_ret
+        result+=f'{animal} say {sound}\n'
+    #print(result)
+    result+=f'Made with {heart} by Antosha\n'
+    return result
 
 # create the Flask app
 app = Flask(__name__)
@@ -31,11 +32,14 @@ def query_example():
     #request_data = request.get_json()
     #print(request_data)
     #print(type(request_data))
-        res = list(request.form.keys())[0]
-        # print(type(res))
-        res2 = json.loads(res)
-        #print(emoji_dict.get(res2['animal'].upper()))
-        return animal_say(emoji_dict.get(res2['animal'].upper()), res2['sound'], res2['count'], emoji_dict.get("SPARKLING HEART"))
+        try:
+            res = list(request.form.keys())[0]
+            # print(type(res))
+            res2 = json.loads(res)
+            #print(emoji_dict.get(res2['animal'].upper()))
+            return animal_say(emoji_dict.get(res2['animal'].upper()), res2['sound'], res2['count'], emoji_dict.get("SPARKLING HEART"))
+        except:
+            return f"""Use this format: {{animal:"dog", sound:"woof", count: 4}}"""+"\n"
     res_text = f"""
     Hello!<br/>
     Use one of these <a href="https://emojipedia.org/nature">emodji</a><br/><br/>
